6c80ddbb1c20b4f094bac09682430879aca4e078
Luis Morales <moralesl@users.noreply.github.com>
2025-02-07 22:43:34 +0100

    Simplify static root

--- django-on-lambda-docker/app/core/settings.py
+++ django-on-lambda-docker/app/core/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 FORCE_SCRIPT_NAME = '/dev'
 
 STATIC_URL = '/dev/static/'
-STATIC_ROOT = os.environ.get('DJANGO_STATIC_ROOT', os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
