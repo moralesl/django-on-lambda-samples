16dc4ae168ef56112cb1013b4e8f60ef37e4fac2
Luis Morales <moralesl@users.noreply.github.com>
2025-02-26 13:47:57 +0100

    Use $default stage

--- django-on-lambda-docker/terraform/main.tf
+++ django-on-lambda-docker/terraform/main.tf
@@ -279,5 +279,5 @@ resource "aws_api_gateway_deployment" "main" {
 resource "aws_api_gateway_stage" "main" {
   deployment_id = aws_api_gateway_deployment.main.id
   rest_api_id  = aws_api_gateway_rest_api.main.id
-  stage_name   = "dev"
+  stage_name   = "$default"
 }
